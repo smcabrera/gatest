eca35abd21045aa399b7b0c7510bdcce21095bff
Stephen Mariano Cabrera <mariano@latinamericaforless.com>
2014-06-17 13:14:32 -0500

    Incluir el js en el mismo archivo para explicar mejor

--- codetest.php
+++ codetest.php
@@ -1,11 +1,10 @@
 <?php
 $pageTitle = "Testing google analytics code";
 include_once("inc/header.php");
-include_once("inc/analyticstracking.php");
 ?>
 
 <script type="text/javascript" charset="utf-8">
-  
+
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
